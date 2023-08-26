41944b1649e6e58e9d33dfc3c4aa8571539ca793
pward70 <pward70@bloomberg.net>
2023-08-26 08:18:54 -0400

    new blackjack example

---
+++ blackjack_plots.py
@@ -0,0 +1,253 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib as mpl
+import copy
+from matplotlib import cm
+import numpy as np
+from typing import Callable, Any, Tuple, Union, Mapping
+
+# Types
+PlayerTotal = int
+DealerCard = int
+UsableAce = bool
+
+State = Tuple[PlayerTotal, DealerCard, UsableAce]
+ActionProb = np.ndarray[float, float]
+
+# Dims
+player_state_space_dim = 32
+action_space_dim = 2
+dealer_state_space_dim = 11
+ace_state_space_dim = 2
+
+def policy_from_Q(Q:dict) -> dict:
+    mc_policy = {}
+    for state, value in Q.items():
+        action = np.argmax(value)
+        action_hit = action
+        action_stay = 1 - action
+        mc_policy[state] = np.array([action_stay, action_hit])
+
+    return mc_policy
+        
+
+def Q_to_V(Q:dict, policy:Union[Callable, Mapping[State, ActionProb]]) -> dict:
+    '''
+    Function to change Q values into V values
+    '''
+    v={}
+    for state, value in Q.items():
+        action_prob = policy[state] if isinstance(policy, dict) else policy(state)
+        v[state] = np.dot(action_prob, value)
+    
+    return v
+
+# changing policy repr
+def change_policy_to_np_array(policy, policy_type: str = "Function") -> np.ndarray:
+    '''change policy to numpy array depending on policy_type'''
+
+    if policy_type == 'dict':
+        return change_dict_policy_to_np_array(policy)
+
+    # assume its a function
+    else:
+        return change_fct_policy_to_np(policy)
+
+def change_fct_policy_to_np(policy: Callable) -> np.ndarray:
+    '''function to change function repr of policy to np arr'''
+
+    policy_arr = np.ones(
+        (player_state_space_dim,
+        dealer_state_space_dim,
+        ace_state_space_dim,
+        action_space_dim,
+        ))
+
+    for i in range(player_state_space_dim):
+        for j in range(dealer_state_space_dim):
+            for k in range(ace_state_space_dim):
+
+                policy_arr[i,j,k] = policy((i, j, k))
+
+    return policy_arr
+
+def change_dict_policy_to_np_array(policy: dict) -> np.ndarray:
+    '''function to change dict repr of policy to np array'''
+
+    policy_arr = np.ones(
+        (player_state_space_dim,
+        dealer_state_space_dim,
+        ace_state_space_dim,
+        action_space_dim
+        ))
+
+    # default to always hit if not seen in dict
+    policy_arr[:, :, :] = [0, 1]
+
+    for state, action_prob in policy.items():
+        player_score, dealer_card, ace = state
+
+        policy_arr[player_score, dealer_card, int(ace)] = action_prob
+
+
+    return policy_arr
+
+def argmax_policy(policy:np.ndarray, policy_type = "Function") -> np.ndarray:
+    '''
+    function to turn policy completely greedy by looking for
+    argmax action over action dimension
+    '''
+
+    return np.argmax(policy, axis = 3)
+
+
+# changing Value function representations
+def change_dict_value_fct_to_np(v: dict) -> np.ndarray:
+    '''
+    Changes state values from dictionaries to np arrays
+    '''
+
+    new_v = np.zeros(
+        (player_state_space_dim,
+        dealer_state_space_dim,
+        ace_state_space_dim
+        ))
+
+    for state, value in v.items():
+
+        player_score, dealer_card, ace = state
+        new_v[player_score, dealer_card, int(ace)] = value
+
+    return new_v
+
+def change_dict_q_value_to_np(q: dict) -> np.ndarray:
+
+    '''
+    Change action-values from dictionaries to np arrays
+    '''
+
+    new_q = np.zeros(
+        (player_state_space_dim,
+        dealer_state_space_dim,
+        ace_state_space_dim,
+        action_space_dim
+        ))
+
+    for state, value in q.items():
+
+        player_score, dealer_card, ace = state
+        new_q[player_score, dealer_card, int(ace), 0] = value[0]
+        new_q[player_score, dealer_card, int(ace), 1] = value[1]
+
+    return new_q
+
+def plot_values(state_val):
+
+    state_val = change_dict_value_fct_to_np(state_val)
+
+    # without usable ace
+    fig, ax = plt.subplots(subplot_kw = {"projection": "3d"}, figsize=(12, 8))
+
+    # specify range of display for player and dealer
+    player_range = np.arange(11, 22)
+    dealer_range = np.arange(1, 11)
+
+    # create a meshgrid of each point in the player_range X dealer_range
+    X, Y = np.meshgrid(dealer_range, player_range)
+
+    # get the Z-coords
+    Z = state_val[11:22, 1:11, 0].reshape(X.shape)  # no usable ace
+
+    # plot x-coords, y-coords, z, coords
+    ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=1,
+                    rstride=1, cstride=1)
+    ax.set_title("State Value ($V_\pi$) Without Ace")
+    ax.set_xlabel("Dealer Showing")
+    ax.set_ylabel("Player Hand")
+    ax.set_zlabel("State Value")
+    plt.show()
+
+    # With usable ace
+    fig, ax = plt.subplots(subplot_kw = {"projection": "3d"}, figsize=(12, 8))
+
+    X, Y = np.meshgrid(dealer_range, player_range)
+
+    Z = state_val[11:22, 1:11, 1].reshape(X.shape)  # usable ace
+
+    ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=1,
+                    rstride=1, cstride=1)
+    ax.set_title("State Value ($V_\pi$) With Ace")
+    ax.set_xlabel("Dealer Showing")
+    ax.set_ylabel("Player Hand")
+    ax.set_zlabel("State Value")
+    plt.show()
+
+def plot_policy(policy: Any):
+
+    if type(policy) != np.ndarray:
+        policy = change_fct_policy_to_np(policy=policy)
+
+    fig, axs, = plt.subplots(1, 2, figsize = (15, 6))
+
+    extent = -1, 9, -1, 11  # the x tick limits
+    x_range = np.arange(10)
+
+    # first plot
+    surf = axs[0].imshow(
+        np.flip(policy[10:22,1:11,0, 1], axis=0),  # flip so that low player card is at the bottom not top
+        cmap=cm.Blues,
+        vmin=0,
+        vmax=1,
+        interpolation='nearest',
+        extent=extent
+    )
+
+    plt.sca(axs[0])  # set current axis
+    plt.xticks(x_range, ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+    plt.yticks(np.arange(12), (str(i) for i in range(10, 22, 1)))
+    axs[0].set_xlabel('Dealer Card Showing')
+    axs[0].set_ylabel('Player Total')
+    axs[0].set_title("Policy ($\pi$) Without Ace")
+    axs[0].grid()
+
+    col_bar = fig.colorbar(surf, ax=axs[0])
+    col_bar.set_ticks([0,0.5, 1])
+    col_bar.set_ticklabels(['Stay (0)', 'Random (0.5)', 'Hit (1)'])
+    col_bar.ax.invert_yaxis()  # flip the y axis
+
+    # second plot
+    surf = axs[1].imshow(
+        np.flip(policy[10:22,1:11,1, 1], axis = 0),  # flip so that low player card is at the bottom not top
+        cmap=cm.Blues,
+        vmin=0,
+        vmax=1,
+        interpolation='nearest',
+        extent=extent
+    )
+
+    plt.sca(axs[1])  # set current axis
+    plt.xticks(x_range, ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
+    plt.yticks(np.arange(12), (str(i) for i in range(10, 22, 1)))
+    axs[1].set_xlabel('Dealer Card Showing')
+    axs[1].set_ylabel('Player Total')
+    axs[1].set_title("Policy ($\pi$) With Ace")
+    axs[1].grid()
+
+    col_bar = fig.colorbar(surf, ax=axs[1])
+    col_bar.set_ticks([0,0.5, 1])
+    col_bar.set_ticklabels(['Stay (0)', 'Random (0.5)', 'Hit (1)'])
+    col_bar.ax.invert_yaxis()  # flip the y axis
+
+    plt.show()
+    
+def dict_Q_values_to_array(Q_values:dict) -> np.ndarray:
+    
+    policy_arr = np.ones((32, 11, 2, 2))
+    for state in Q_values:
+        player_score, dealer_card, ace = state
+        action = np.argmax(Q_values[state])
+        action_hit = action
+        action_stay = 1 - action
+        policy_arr[player_score, dealer_card, int(ace)] = [action_stay, action_hit]
+        
+    return policy_arr
